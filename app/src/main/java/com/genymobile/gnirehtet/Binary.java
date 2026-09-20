7b7c29b29b0be58619fac6e2b32ab85f2ad2f728
Engineer Purim <128815330+purim-sudo@users.noreply.github.com>
2026-09-20 17:57:04 +0300

    Reconstruct NetBridgeX source: app/src/main/java/com/genymobile/gnirehtet/Binary.java

---
+++ app/src/main/java/com/genymobile/gnirehtet/Binary.java
@@ -0,0 +1,42 @@
+[Reading 38 lines from start (total: 38 lines, 0 remaining)]
+
+package com.genymobile.gnirehtet;
+
+/* JADX INFO: loaded from: classes2.dex */
+public final class Binary {
+    private static final int MAX_STRING_PACKET_SIZE = 20;
+
+    private Binary() {
+    }
+
+    public static int unsigned(byte value) {
+        return value & 255;
+    }
+
+    public static int unsigned(short value) {
+        return 65535 & value;
+    }
+
+    public static long unsigned(int value) {
+        return ((long) value) & 4294967295L;
+    }
+
+    public static String buildPacketString(byte[] data, int len) {
+        int limit = Math.min(MAX_STRING_PACKET_SIZE, len);
+        StringBuilder builder = new StringBuilder();
+        builder.append('[').append(len).append(" bytes] ");
+        for (int i = 0; i < limit; i++) {
+            if (i != 0) {
+                String sep = i % 4 == 0 ? "  " : " ";
+                builder.append(sep);
+            }
+            builder.append(String.format("%02X", Integer.valueOf(data[i] & 255)));
+        }
+        if (limit < len) {
+            builder.append(" ... +").append(len - limit).append(" bytes");
+        }
+        return builder.toString();
+    }
+}
+
+[executed on device: DESKTOP-1RJKODI (291d4b53-196c-4a98-a307-e51f238e5e74)]
